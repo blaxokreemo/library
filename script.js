8214ce33efccd39d3219103eed927d6f307cf7fe
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-08-27 22:12:04 -0400

    Create basic JavaScript functionality to add books and push them onto the grid in cards

--- script.js
+++ script.js
@@ -13,3 +13,71 @@ function addBookToLibrary(book) {
     // do stuff here
     myLibrary.push(book);
 }
+
+
+const form = document.getElementById('new-book-form');
+const library = document.getElementById('library');
+
+form.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+    let title = form.elements['title'].value
+    let author = form.elements['author'].value;
+    let pages = form.elements['pages'].value;
+    let read = form.elements['read'].value;
+
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+
+    form.elements['title'].value = '';
+    form.elements['author'].value = '';
+    form.elements['pages'].value = '';
+
+    let bookCards = library.querySelectorAll('.book');
+    bookCards.forEach((card) => {
+        library.removeChild(card);
+    })
+
+    myLibrary.forEach((book) => {
+        createBookCard(book);
+    })
+
+})
+
+function newBookTest(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+    console.log(myLibrary);
+}
+
+function createBookCard(bookObject) {
+
+    let newBookCard = document.createElement('div');
+    newBookCard.classList.add('book');
+    
+    let titleHeader = document.createElement('h3');
+    titleHeader.classList.add('title-header');
+    titleHeader.textContent = bookObject['title'];
+
+    let authorHeader = document.createElement('h3');
+    authorHeader.classList.add('author-header');
+    authorHeader.textContent = `by ${bookObject['author']}`;
+
+    let pagesHeader = document.createElement('h4');
+    pagesHeader.classList.add('pages-header');
+    pagesHeader.textContent = `${bookObject['pages']} pages`;
+
+    let readHeader = document.createElement('h4');
+    readHeader.classList.add('read-header');
+    readHeader.textContent = bookObject['read'];
+
+    newBookCard.appendChild(titleHeader);
+    newBookCard.appendChild(authorHeader);
+    newBookCard.appendChild(pagesHeader);
+    newBookCard.appendChild(readHeader);
+    
+    library.appendChild(newBookCard);
+
+
+}
